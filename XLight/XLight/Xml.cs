622c4bbd131145d9222afb11badeb60dc49e204b
Moon Antonio <pinchodev91@gmail.com>
2017-10-06 00:43:05 +0200

    Add metodo crear xml

--- XLight/XLight/Xml.cs
+++ XLight/XLight/Xml.cs
@@ -7,10 +7,15 @@
 // Ultima Mod:      Version Inicial												\\
 //******************************************************************************\\
 
+#region Librerias
 using System.Xml;
+#endregion
 
 namespace XLight
 {
+	/// <summary>
+	/// <para>Clase para controlar XML</para>
+	/// </summary>
 	class Xml
 	{
 		#region Variables Privadas
@@ -18,7 +23,59 @@ namespace XLight
 		/// <para>Ruta del archivo xml de origen.</para>
 		/// </summary>
 		private string rutaXML;											// Ruta del archivo xml de origen
-		private XmlDocument doc;
+		/// <summary>
+		/// <para>Documento</para>
+		/// </summary>
+		private XmlDocument doc;                                        // Documento
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Crear un archivo xml en la ruta dada.</para>
+		/// </summary>
+		/// <param name="ruta"></param>
+		/// <param name="nodoRaiz"></param>
+		public void CrearXML(string ruta, string nodoRaiz)// Crear un archivo xml en la ruta dada
+		{
+			this.rutaXML = ruta;
+			doc = new XmlDocument();
+
+			XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0.0", "UTF-8", null);
+			XmlNode root = doc.DocumentElement;
+			doc.InsertBefore(xmlDeclaration, root);
+
+			XmlNode elemento = doc.CreateElement(nodoRaiz);
+			doc.AppendChild(elemento);
+			doc.Save(ruta);
+		}
+
+		/// <summary>
+		/// <para>Agrega un elemento al historial.</para>
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="nom"></param>
+		/// <param name="tipo"></param>
+		/// <param name="fecha"></param>
+		/// <param name="precio"></param>
+		/// <param name="resultado"></param>
+		public void AgregarHistorial(string id, string nom, TiposTratamiento tipo, string fecha, string precio, string resultado)// Agrega un elemento al historial
+		{
+
+		}
+		#endregion
+
+		#region Enum
+		/// <summary>
+		/// <para>Tipos de tratamientos</para>
+		/// </summary>
+		public enum TiposTratamiento
+		{
+			Interpretar,
+			Regresion,
+			TetraHealing,
+			Reiki,
+			Hipnosis
+		}
 		#endregion
 	}
 }
